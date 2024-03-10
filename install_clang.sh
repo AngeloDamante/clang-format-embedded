4eec7984b1cfc0ffb88e04840a29932159f92866
AngeloDamante <angelo.damante16@gmail.com>
2024-03-10 17:19:50 +0100

    build: update installer

--- install_clang.sh
+++ install_clang.sh
@@ -2,13 +2,20 @@
 # @details: Install clang format 13
 # @file: install_clang.sh
 
-if [ -d /usr/lib/clang ]; then
-	echo "Directory Exists"
+if [ -d /usr/lib/clangone ]; 
+then
+	echo "[LOG] Clang Already Exists"
+	sudo apt-get remove clang 
+	sudo apt-get remove --auto-remove clang 	
+	sudo apt-get purge clang 
+	sudo apt-get purge --auto-remove clang 
+	echo "[LOG] Clang Removed"
+else 
+	echo "[LOG] Installing Clang"
+	sudo apt install clang-13
+	sudo apt install clang-format-13
+	echo "[LOG] Clang Installed"
 fi
 
-# sudo apt-get remove clang 
-# sudo apt-get remove --auto-remove clang 
-# sudo apt-get purge clang 
-# udo apt-get purge --auto-remove clang 
 
 # clang-format-13 -style=llvm -dump-config > formatter.clang-format
